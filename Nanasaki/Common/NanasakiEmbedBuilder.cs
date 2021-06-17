43cdc16d879751ad360c53a3348f31993e829dc0
NinjaZ <ninjaz.gg@outlook.com>
2021-06-17 12:22:03 -0400

    Add project files.

---
+++ Nanasaki/Common/NanasakiEmbedBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanasaki.Common
+{
+	/// <summary>
+	/// Custom embed builder theme for Nanasaki
+	/// </summary>
+	internal class NanasakiEmbedBuilder : EmbedBuilder
+	{
+		public NanasakiEmbedBuilder()
+		{
+			this.WithColor(new Color(138, 118, 118));
+		}
+	}
+}
---
+++ Nanasaki/Modules/General.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Nanasaki.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanasaki.Modules
+{
+
+	/// <summary>
+	/// The general module containing commands
+	/// </summary>
+	public class General : ModuleBase<SocketCommandContext>
+	{
+
+		/// <summary>
+		/// Responds with "Pong!".
+		/// </summary>
+		/// <returns></returns>
+		[Command("ping")]
+		public async Task PingAsync()
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await Context.Channel.SendMessageAsync("Pong!");
+		}
+
+		/// <summary>
+		/// Gets information about a user.
+		/// </summary>
+		/// <param name="socketGuildUser">An optional user to get the information from.</param>
+		/// <returns></returns>
+		[Command("info")]
+		public async Task InfoAsync(SocketGuildUser socketGuildUser = null)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			if (socketGuildUser == null)
+			{
+				socketGuildUser = Context.User as SocketGuildUser;
+			}
+
+			var embed = new NanasakiEmbedBuilder()
+				.WithTitle($"{socketGuildUser.Username}#{socketGuildUser.Discriminator}")
+				.AddField("ID", socketGuildUser.Id, true)
+				.AddField("Name", $"{socketGuildUser.Username}#{socketGuildUser.Discriminator}", true)
+				.AddField("Created at", socketGuildUser.CreatedAt.ToString("yyyy年M月d日"), true)
+				.WithThumbnailUrl(socketGuildUser.GetAvatarUrl() ?? socketGuildUser.GetDefaultAvatarUrl())
+				.WithCurrentTimestamp()
+				.Build();
+
+			await this.ReplyAsync(embed: embed);
+		}
+	}
+}
