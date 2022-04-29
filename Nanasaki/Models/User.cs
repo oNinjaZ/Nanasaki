87bc604e8853d216ae51882bb4dc567d6c6a5922
ninjaz.jp@outlook.com <marcbelzamour@yahoo.com>
2022-04-29 17:45:31 -0400

    add user model

---
+++ Nanasaki/Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nanasaki.Models;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    
+}
