793b0562a2217b27a14a05a337a27eaeb3a80c9c
ninjaz.jp@outlook.com <marcbelzamour@yahoo.com>
2022-04-29 18:09:29 -0400

    add default init

--- Nanasaki/Models/User.cs
+++ Nanasaki/Models/User.cs
@@ -4,8 +4,8 @@ namespace Nanasaki.Models;
 
 public class User
 {
-    public string Id { get; set; }
-    public string Username { get; set; }
+    public string Id { get; set; } = default!;
+    public string Username { get; set; } = default!;
     public DateTime RegistrationDate { get; set; }
     
 }
