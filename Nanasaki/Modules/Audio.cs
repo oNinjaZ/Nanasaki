d4db5c667c41f46cf352b1326e2ce2560433cfca
ninjaz.jp@outlook.com <marcbelzamour@yahoo.com>
2022-04-29 16:15:59 -0400

    cmt

--- Nanasaki/Modules/Audio.cs
+++ Nanasaki/Modules/Audio.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
+using Discord.Audio;
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,26 +12,61 @@ namespace Nanasaki.Modules
 {
     public class Audio : ModuleBase<SocketCommandContext>
     {
-		[Command("join", RunMode = RunMode.Async)]
-		public async Task JoinChannel(IVoiceChannel channel = null)
-		{
-			// Get the audio channel
-			channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
-			if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
 
-			// For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
-			var audioClient = await channel.ConnectAsync();
-		}
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await channel.ConnectAsync();
+        }
 
-		[Command("leave", RunMode = RunMode.Async)]
-		public async Task LeaveChannel(IVoiceChannel channel = null)
-		{
-			// Get the audio channel
-			channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
-			if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+        [Command("leave", RunMode = RunMode.Async)]
+        public async Task LeaveChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
 
-			// For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
-			await channel.DisconnectAsync();
-		}
-	}
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            await channel.DisconnectAsync();
+        }
+
+        [Command("play")]
+        public async Task PlaySong(IVoiceChannel channel = null)
+        {
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            var audioClient = await channel.ConnectAsync();
+
+			await SendAsync(audioClient, @"D:\C#\Discord\Nanasaki\Nanasaki\Assets\Music\coldgreeneyes.mp3");
+            
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg using the previous example
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+    }
 }
