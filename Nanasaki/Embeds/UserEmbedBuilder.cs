e171071232340abeb851f5709d3c4391215bf956
ninjaz.jp@outlook.com <marcbelzamour@yahoo.com>
2022-05-01 17:39:21 -0400

    remove UserEmbedExtensions

--- Nanasaki/Embeds/UserEmbedBuilder.cs
+++ Nanasaki/Embeds/UserEmbedBuilder.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Emit;
 using Discord;
 using Nanasaki.Common;
 
@@ -8,5 +9,13 @@ public class UserEmbedBuilder : NanasakiEmbedBuilder
     public UserEmbedBuilder()
     {
 
+    }
+
+    public UserEmbedBuilder WithHelpCommands()
+    {
+        this.AddField("-user register <username>", "Create an account (1 max). <username> parameter is optional.")
+            .AddField("-user change <new username>", "Change your username."); 
+        
+        return this; 
     }
 }
